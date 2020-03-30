dc7836a35210d28040cd236f20157a623b5bd275
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-30 19:17:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,11 +18,11 @@ attendees.each_with_index.map do |attendee, index|
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each. do |badge| 
-    
+  batch_badge_creator(attendees).each do |badge| 
+    puts badge
 end
 
-assign_rooms(attendees).each do |assignment|
+  assign_rooms(attendees).each do |assignment|
   puts assignment
   end
 end
