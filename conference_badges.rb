d0233b50dc321cf71f87f48a8dcf4333a3b41636
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-30 19:10:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,14 @@
 # Write your code here.
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def badge_maker(name)
-  "Hello, my name is #{name}."
+def badge_maker(attendee)
+  "Hello, my name is #{attendee}."
 end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
     "Hello, my name is #{attendee}."
+  end
 end
 
 def assign_rooms(attendees)
